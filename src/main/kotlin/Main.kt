315a24d3a6ce13958d52a76d880194b58ec45c18
IngFabian97 <fabian.guarin@hotmail.com>
2024-09-17 20:18:59 -0500

    Parte 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,42 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+
+    var accountType: String = ""
+    var userChoice: Int = 0
+
+    println("Welcome to your banking system.")
+    println("What type of account would you like to create?")
+    println("1. Debit account")
+    println("2. Credit account")
+    println("3. Checking account")
+
+    while (accountType == ""){
+        println("Choose an option (1,2 or 3)")
+        userChoice = (1..5).random()
+        println("The selected option is...$userChoice")
+
+        when {
+            userChoice <= 1 ->
+                {
+                    accountType = "debit"
+                }
+            userChoice <= 2 ->
+                {
+                    accountType = "credit"
+                }
+            userChoice <= 3 ->
+                {
+                    accountType = "checking"
+                }
+            else ->
+                {
+                    continue
+                }
+        }
+
+        println("You have created a $accountType account")
+     }
+
+
 }
